d03316a61368acf69dc1d68de9a58b972fd61f7d
LucasNT <lucasntakahashi@gmail.com>
2025-11-16 18:02:44 -0300

    feat(nix): update flake to have  the build config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Teste do google oauth2";
+  description = "Golang Template";
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05"; };
 
@@ -9,9 +9,21 @@
       system = "x86_64-linux";
       # use it here, and bind platform-specific packages to `pkgs`
       pkgs = nixpkgs.legacyPackages.${system};
+      nativeBuildInputs = with pkgs; [ go ];
+      developmentPakcages = with pkgs; [ gopls delve ];
     in {
+
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "golang google oauth tester" ;
+        version = "1";
+        src = ./.;
+        vendorHash = "sha256-JFxW88GoE/oWL/JFcZZxL8AHQJr5O/cz1OoINzFS0Tk=";
+        # subPackages = [ "path1", "path2" ]
+        env = { CGO_ENABLED = 0; };
+      };
       devShells.${system}.default = pkgs.mkShell {
-        packages = [ pkgs.go pkgs.gopls pkgs.bashInteractive pkgs.delve ];
+        packages = nativeBuildInputs ++ developmentPakcages
+          ++ [ pkgs.bashInteractive ];
       };
     };
 }
